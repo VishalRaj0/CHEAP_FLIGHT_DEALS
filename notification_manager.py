86e60ed0516292f006e143d1ebfc5ba3a05f1378
VishalRaj0 <108023331+VishalRaj0@users.noreply.github.com>
2024-03-11 23:14:49 +0530

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,15 +1,17 @@
 from twilio.rest import Client
 
-ACCOUNT_SID = "ACa24898c0f17645d4382da0666b4354e7"
-AUTH_TOKEN = "33eb031b88da60aa5cb28ecfc236f76b"
+ACCOUNT_SID = "YOUR TWILIO ACCOUNT SID"
+AUTH_TOKEN = "YOUR TWILIO AUTHORIZATION TOKEN"
+TWILIO_NUMBER = ""
+RECIEVER_NUMBER = ""
 
 CLIENT = Client(ACCOUNT_SID, AUTH_TOKEN)
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
     def send_notification(self, **kwargs):
         message = CLIENT.messages.create(
-            from_="+19049005420",
-            to="+917769932142",
+            from_=TWILIO_NUMBER,
+            to=RECIEVER_NUMBER,
             body=f"Price: EUR {kwargs['price']} \n"
                  f"Departure from: {kwargs['city_from']} ({kwargs['city_code_from']}) \n"
                  f"Arrival at: {kwargs['to']['city']} ({kwargs['to']['iataCode']}) \n"
@@ -19,8 +21,8 @@ class NotificationManager:
 
     def flight_not_found_notification(self):
         message = CLIENT.messages.create(
-            from_="+19049005420",
-            to="+917769932142",
+            from_=TWILIO_NUMBER,
+            to=RECIEVER_NUMBER,
             body=f"Sorry! The flight that you requested does not exist "
                  f"or has no recurring flights in the next 6 months."
         )
